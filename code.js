6d2bbdf11125e0676025fe50a2bff782e73a61e6
ElvinCa <lil51carias@gmail.com>
2023-05-13 22:16:43 -0600

    Primer challenge alura oracle one

---
+++ code.js
@@ -0,0 +1,68 @@
+const textArea = document.querySelector("#encriptador");
+const mensaje = document.querySelector("#mensaje-encriptado");
+const textoBr = document.querySelector(".pbr");
+const textoBr2 = document.querySelector(".pbr2");
+
+
+/*
+La letra "e" es convertida para "enter"
+La letra "i" es convertida para "imes"
+La letra "a" es convertida para "ai"
+La letra "o" es convertida para "ober"
+La letra "u" es convertida para "ufat" */
+
+function btnEncriptar(){
+    const textoEncriptado = encriptar(textArea.value);
+    mensaje.value = textoEncriptado;
+    textArea.value = "";
+    mensaje.style.backgroundImage = "none";
+    textoBr.style.visibility = "hidden";
+    textoBr2.style.visibility = "hidden";
+}
+
+function encriptar(stringEncriptado){
+    let matrizCodigo = [["e", "enter"], ["i", "imes"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]];
+    stringEncriptado = stringEncriptado.toLowerCase();
+
+    for(let i = 0; i < matrizCodigo.length; i++){
+        if(stringEncriptado.includes(matrizCodigo[i][0])){
+            stringEncriptado = stringEncriptado.replaceAll(matrizCodigo[i][0], matrizCodigo[i][1]);
+        }
+    }
+    return stringEncriptado;
+}
+
+//desencriptado
+
+function desencriptar (stringDesencriptado){
+    let matrizCodigo = [["e", "enter"], ["i", "imes"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]];
+    stringDesencriptado = stringDesencriptado.toLowerCase();
+
+    for(let i = 0; i < matrizCodigo.length; i++){
+        if (stringDesencriptado.includes(matrizCodigo[i][1])){
+            stringDesencriptado = stringDesencriptado.replaceAll(matrizCodigo[i][1], matrizCodigo[i][0]);
+        }
+    }
+    return stringDesencriptado;
+}
+
+function btnDesencriptar (){
+    const textoDesencriptado = desencriptar(textArea.value);
+    mensaje.value = textoDesencriptado;
+    textArea.value = "";
+    mensaje.style.backgroundImage = "none";
+}
+
+//copiar texto
+
+function copiarTexto() {
+    let texto = document.getElementById("mensaje-encriptado").value;
+    navigator.clipboard.writeText(texto)
+      .then(function() {
+        console.log("Texto copiado al portapapeles");
+      })
+      .catch(function(error) {
+        console.error("Error al copiar el texto: ", error);
+      });
+  }
+  
